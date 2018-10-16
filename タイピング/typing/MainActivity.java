a333abd707ef38232fd5ff4c5ea00ba7561919f6
chika7271994 <43559639+chika7271994@users.noreply.github.com>
2018-10-16 10:41:30 +0900

    Add files via upload

---
+++ タイピング/typing/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.typing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TestOpenHelper helper;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // DB作成
+        helper = new TestOpenHelper(getApplicationContext());
+
+        Button readButton = findViewById(R.id.button);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readData();
+            }
+        });
+
+        textView = findViewById(R.id.text_view);
+
+    }
+
+    private void readData(){
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.query(
+                "testdb",
+                new String[] { "items", "price" },
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToFirst();
+
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+            sbuilder.append(cursor.getString(0));
+            sbuilder.append(":    ");
+            sbuilder.append(cursor.getInt(1));
+            sbuilder.append("yen\n\n");
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+        textView.setText(sbuilder.toString());
+    }
+}
+
+
